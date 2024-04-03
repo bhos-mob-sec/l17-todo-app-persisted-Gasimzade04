b9b8dc0a6f9f85b0f9806d0ea94c4f76329ace26
Gasimzade04 <revan.gasimzade04@gmail.com>
2024-04-03 11:46:30 +0400

    commit

--- app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
+++ app/src/main/java/az/edu/bhos/l14todoapp/data/TodoDAO.kt
@@ -1,8 +1,17 @@
 package az.edu.bhos.l14todoapp.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import az.edu.bhos.l14todoapp.data.dto.TodoLocalDto
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
-    // TODO: fill the required methods
+    @Query("SELECT * FROM todos ORDER BY weekday")
+    fun getTodosOrderedByWeekday(): Flow<List<TodoLocalDto>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(todos: List<TodoLocalDto>)
 }
--- app/src/main/java/az/edu/bhos/l14todoapp/data/dto/TodoLocalDto.kt
+++ app/src/main/java/az/edu/bhos/l14todoapp/data/dto/TodoLocalDto.kt
@@ -2,10 +2,12 @@ package az.edu.bhos.l14todoapp.data.dto
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
 
-// todo complete the entity class
 @Entity(tableName = "todos")
 data class TodoLocalDto(
-    @PrimaryKey
-    val id: String
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "completed") val completed: Boolean,
+    @ColumnInfo(name = "weekday") val weekday: String
 )
